09b072dc35948094f5e5ff1e6b67fc78dcea62a5
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2018-11-13 17:00:00 +0000

    Added mis rules (#20)
    * Added mis rules
    
    * Added mis rules v1

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -66,6 +66,26 @@ resource "aws_security_group_rule" "bastion_in" {
   ]
 }
 
+resource "aws_security_group_rule" "jump_http" {
+  security_group_id        = "${local.sg_mis_jumphost}"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  type                     = "egress"
+  description              = "${local.common_name}-http-out"
+  source_security_group_id = "${local.sg_mis_common}"
+}
+
+resource "aws_security_group_rule" "jump_http_alt" {
+  security_group_id        = "${local.sg_mis_jumphost}"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  type                     = "egress"
+  description              = "${local.common_name}-http-alt-out"
+  source_security_group_id = "${local.sg_mis_common}"
+}
+
 resource "aws_security_group_rule" "bastion_egress" {
   security_group_id        = "${local.sg_mis_jumphost}"
   from_port                = 22
@@ -79,16 +99,6 @@ resource "aws_security_group_rule" "bastion_egress" {
 #-------------------------------------------------------------
 ### app
 #-------------------------------------------------------------
-
-resource "aws_security_group_rule" "tomcat_in" {
-  security_group_id        = "${local.sg_mis_app_in}"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  type                     = "ingress"
-  description              = "${local.common_name}-tomcat-in"
-  source_security_group_id = "${local.sg_mis_jumphost}"
-}
 
 #-------------------------------------------------------------
 ### common sg rules
@@ -110,6 +120,26 @@ resource "aws_security_group_rule" "ssh_in" {
   protocol                 = "tcp"
   type                     = "ingress"
   description              = "${local.common_name}-rdp-in"
+  source_security_group_id = "${local.sg_mis_jumphost}"
+}
+
+resource "aws_security_group_rule" "http_alt_in" {
+  security_group_id        = "${local.sg_mis_common}"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  type                     = "ingress"
+  description              = "${local.common_name}-http-alt-in"
+  source_security_group_id = "${local.sg_mis_jumphost}"
+}
+
+resource "aws_security_group_rule" "http_in" {
+  security_group_id        = "${local.sg_mis_common}"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  type                     = "ingress"
+  description              = "${local.common_name}-http-in"
   source_security_group_id = "${local.sg_mis_jumphost}"
 }
 
