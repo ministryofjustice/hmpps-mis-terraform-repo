de48e23d26ad26767ee33bd45423074a714bf6ac
Brett Minnie <github@bdb-studios.co.uk>
2018-11-22 16:42:28 +0000

    Updated bootstrap

--- smtp-server/user_data/bootstrap.sh
+++ smtp-server/user_data/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set +x
+
 yum install -y python-pip git wget
 
 cat << EOF >> /etc/environment
@@ -44,18 +46,16 @@ cat << EOF > ~/bootstrap.yml
     - "~/users.yml"
   roles:
      - bootstrap
-     - rsyslog
-     - elasticbeats
      - users
 
 EOF
 
-ansible-galaxy install -r ~/requirements.txt
+ansible-galaxy install -r ~/requirements.yml
 SELF_REGISTER=true ansible-playbook ~/bootstrap.yml
 
-mkdir -p /srv/service-postix
+mkdir -p /srv/service-postfix
 
-cat << EOF > /etc/service-postfix/docker-compose.yml
+cat << EOF > /srv/service-postfix/docker-compose.yml
 
 version: '3'
 services:
@@ -66,32 +66,8 @@ services:
     ports:
      - "25:25"
     environment:
-     #  # MUST start with : e.g RELAY_NETWORKS: :192.168.0.0/24:10.0.0.0/16
-     #  # if acting as a relay this or RELAY_DOMAINS must be filled out or incoming mail will be rejected
-     #  RELAY_NETWORKS: :192.168.0.0/24
-     #  # what domains should be accepted to forward to lower distance MX server.
-     #  RELAY_DOMAINS: <domain1> : <domain2> : <domain3>
-     #  # To act as a Gmail relay
-     #  GMAIL_USER:
-     #  GMAIL_PASSWORD:
-     #  # For use with Amazon SES relay
-     #  SES_USER:
-     #  SES_PASSWORD:
-     #  SES_REGION:
-     #  # if provided will enable TLS support
-     #  KEY_PATH:
-     #  CERTIFICATE_PATH:
-     #  # the outgoing mail hostname
        MAILNAME: ${mail_hostname}
-     #  # set this to any value to disable ipv6
-     #  DISABLE_IPV6:
-     # # Generic SMTP Relay
-     # SMARTHOST_ADDRESS:
-     # SMARTHOST_PORT:
-     # SMARTHOST_USER:
-     # SMARTHOST_PASSWORD:
-     # SMARTHOST_ALIASES:
 
 EOF
 
-docker-compose up -f /srv/service-postix/docker-compose.yml -d
+docker-compose up -f /srv/service-postfix/docker-compose.yml -d
