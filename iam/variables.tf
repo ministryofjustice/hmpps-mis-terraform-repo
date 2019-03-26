357aa4d7e73c6414db14d107a0d78d9f9ed0905b
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-03-26 15:49:43 +0000

    Added JenkinsFile

--- iam/variables.tf
+++ iam/variables.tf
@@ -10,8 +10,4 @@ variable "environment_type" {
   description = "environment"
 }
 
-variable "eng-remote_state_bucket_name" {
-  description = "Terraform remote state bucket name"
-}
-
 variable "cross_account_iam_role" {}
