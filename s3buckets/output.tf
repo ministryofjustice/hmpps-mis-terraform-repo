7cf2ba76bfa4ffd79a46398287f0b5263bf89c92
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-03-28 11:18:07 +0000

    creating mis s3 backup bucket

--- s3buckets/output.tf
+++ s3buckets/output.tf
@@ -5,3 +5,11 @@
 output "s3bucket" {
   value = "${module.s3bucket.s3bucket}"
 }
+
+output "s3bucket-backups" {
+  value = "${module.s3bucket-backups.s3_bucket_name}"
+}
+
+output "s3_bucket_backups_arn" {
+  value = "${module.s3bucket-backups.s3_bucket_arn}"
+}
