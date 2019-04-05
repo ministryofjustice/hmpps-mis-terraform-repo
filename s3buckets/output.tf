4c3f79dccd44c1ab82e07313633ec3f417d3dd4c
smjmoj <smjmoj@gmail.com>
2019-04-05 19:01:58 +0100

    Updated backups S3 bucket output names to make it easier to understand at point of use elsewhere in the code base

--- s3buckets/output.tf
+++ s3buckets/output.tf
@@ -6,10 +6,10 @@ output "s3bucket" {
   value = "${module.s3bucket.s3bucket}"
 }
 
-output "s3bucket_backups" {
+output "s3bucket_backups_name" {
   value = "${module.s3bucket-backups.s3_bucket_name}"
 }
 
-output "s3_bucket_backups_arn" {
+output "s3bucket_backups_arn" {
   value = "${module.s3bucket-backups.s3_bucket_arn}"
 }
