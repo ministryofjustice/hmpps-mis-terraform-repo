55846a54a12473f27b99138963b19e3cd01b4e52
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-04-10 13:08:50 +0100

    Added health check

--- security-groups/app-lb-rules.tf
+++ security-groups/app-lb-rules.tf
@@ -1,3 +1,7 @@
+locals {
+  bws_port = "8080"
+}
+
 # app lb
 resource "aws_security_group_rule" "lb_https_in" {
   security_group_id = "${local.sg_mis_app_lb}"
@@ -10,4 +14,24 @@ resource "aws_security_group_rule" "lb_https_in" {
   cidr_blocks = [
     "${local.user_access_cidr_blocks}",
   ]
+}
+
+resource "aws_security_group_rule" "bws_lb_http_egress" {
+  security_group_id        = "${local.sg_mis_app_lb}"
+  from_port                = "${local.bws_port}"
+  to_port                  = "${local.bws_port}"
+  protocol                 = "tcp"
+  type                     = "egress"
+  description              = "${local.common_name}-bws-http-out"
+  source_security_group_id = "${local.sg_mis_app_in}"
+}
+
+resource "aws_security_group_rule" "bws_lb_http_in" {
+  security_group_id        = "${local.sg_mis_app_in}"
+  from_port                = "${local.bws_port}"
+  to_port                  = "${local.bws_port}"
+  protocol                 = "tcp"
+  type                     = "ingress"
+  description              = "${local.common_name}-bws-http-in"
+  source_security_group_id = "${local.sg_mis_app_lb}"
 }
