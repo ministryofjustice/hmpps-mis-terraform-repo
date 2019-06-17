26135fda734721e5a169f48703335750089100b3
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-06-17 14:28:26 +0200

    removed non required sg group rules

--- security-groups/smtp-relay-rules.tf
+++ security-groups/smtp-relay-rules.tf
@@ -1,15 +1,3 @@
-resource "aws_security_group_rule" "smtp_in" {
-  from_port          = 25
-  protocol           = "tcp"
-  security_group_id  = "${local.sg_mis_common}"
-  to_port            = 25
-  type               = "ingress"
-  description        = "${local.environment_identifier}-smtp"
-  cidr_blocks = [
-    "${local.private_cidr_block}",
-  ]
-}
-
 resource "aws_security_group_rule" "ses_out" {
   from_port          = 587
   protocol           = "tcp"
@@ -20,17 +8,5 @@ resource "aws_security_group_rule" "ses_out" {
 
   cidr_blocks = [
     "0.0.0.0/0",
-  ]
-}
-
-resource "aws_security_group_rule" "smtp_host_relay" {
-  from_port          = 25
-  protocol           = "tcp"
-  security_group_id  = "${local.sg_outbound_id}"
-  to_port            = 25
-  type               = "egress"
-  description        = "${local.environment_identifier}-smtp"
-  cidr_blocks = [
-    "${local.private_cidr_block}",
   ]
 }
