a0b05d9f0cc7506c97efa148d761d27189cf63bf
John Barber <jsbarber@gmail.com>
2019-08-14 15:14:49 +0100

    update outputs for 2nd and 3rd instance details

--- ec2-ndl-bps/output.tf
+++ ec2-ndl-bps/output.tf
@@ -15,3 +15,39 @@ output "primary_dns" {
 output "primary_dns_ext" {
   value = "${aws_route53_record.instance_ext.fqdn}"
 }
+
+# secondary ec2
+output "secondary_instance_id" {
+  value = "${module.create-ec2-instance-002.instance_id}"
+}
+
+output "secondary_private_ip" {
+  value = "${module.create-ec2-instance-002.private_ip}"
+}
+
+# dns
+output "secondary_dns" {
+  value = "${aws_route53_record.instance_002.*.fqdn}"
+}
+
+output "secondary_dns_ext" {
+  value = "${aws_route53_record.instance_ext_002.*.fqdn}"
+}
+
+# tertiary ec2
+output "tertiary_instance_id" {
+  value = "${module.create-ec2-instance-003.instance_id}"
+}
+
+output "tertiary_private_ip" {
+  value = "${module.create-ec2-instance-003.private_ip}"
+}
+
+# dns
+output "tertiary_dns" {
+  value = "${aws_route53_record.instance_003.*.fqdn}"
+}
+
+output "tertiary_dns_ext" {
+  value = "${aws_route53_record.instance_ext_003.*.fqdn}"
+}
