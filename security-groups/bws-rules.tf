63f1973bfec8b9eab0ccaea32345cf4b06d94701
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-09-16 13:57:01 +0200

    update ldap ports

--- security-groups/bws-rules.tf
+++ security-groups/bws-rules.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "bws_ldap_in" {
   security_group_id        = "${data.terraform_remote_state.delius_core_security_groups.sg_apacheds_ldap_private_elb_id}"
-  from_port                = 10389
-  to_port                  = 10389
+  from_port                = 389
+  to_port                  = 389
   protocol                 = "tcp"
   type                     = "ingress"
   description              = "bws in to ldap lb"
@@ -10,8 +10,8 @@ resource "aws_security_group_rule" "bws_ldap_in" {
 
  resource "aws_security_group_rule" "bws_ldap_out" {
   security_group_id        = "${data.terraform_remote_state.security-groups.sg_bws_ldap}"
-  from_port                = 10389
-  to_port                  = 10389
+  from_port                = 389
+  to_port                  = 389
   protocol                 = "tcp"
   type                     = "egress"
   description              = "bws out to ldap lb"
