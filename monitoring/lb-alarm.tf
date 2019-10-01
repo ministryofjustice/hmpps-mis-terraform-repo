57167abdacc3a3554677a96eeb47239d28a476b0
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-10-01 14:54:22 +0200

    adding mis monitoringv1

--- monitoring/lb-alarm.tf
+++ monitoring/lb-alarm.tf
@@ -16,18 +16,18 @@ resource "aws_cloudwatch_metric_alarm" "bws_lb_unhealthy_hosts" {
 }
 
 
-
-resource "aws_cloudwatch_metric_alarm" "bws_lb_healthy_hosts" {
-  alarm_name                = "${local.bws_lb_name}-lb-healthy-hosts-count"
-  comparison_operator       = "LessThanThreshold"
+resource "aws_cloudwatch_metric_alarm" "bws_lb_latency" {
+  alarm_name                = "${local.bws_lb_name}-lb-latency"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
-  metric_name               = "HealthyHostCount"
+  metric_name               = "Latency"
   namespace                 = "AWS/ELB"
   period                    = "300"
   statistic                 = "Average"
-  threshold                 = "2"
-  alarm_description         = "This metric monitors BWS lb unhealthy host count"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors BWS lb Latency"
   alarm_actions             = [ "${aws_sns_topic.alarm_notification.arn}" ]
+  treat_missing_data        = "notBreaching"
 
   dimensions {
               LoadBalancerName  = "${local.bws_lb_name}"
--- monitoring/main.tf
+++ monitoring/main.tf
@@ -95,10 +95,74 @@ data "terraform_remote_state" "ec2-ndl-bcs" {
 }
 
 
-
 locals {
   environment_name             = "${var.environment_type}"
   environment_identifier       = "${data.terraform_remote_state.common.short_environment_identifier}"
   mis_app_name                 = "${data.terraform_remote_state.common.mis_app_name}"
   bws_lb_name                  = "${data.terraform_remote_state.ec2-ndl-bws.bws_elb_name}"
+}
+
+#dashboard
+
+resource "aws_cloudwatch_dashboard" "mis" {
+  dashboard_name = "mis-${local.environment_name}-monitoring"
+  dashboard_body = "${data.template_file.dashboard-body.rendered}"
+}
+
+data "template_file" "dashboard-body" {
+  template = "${file("dashboard.json")}"
+  vars {
+    region            = "${var.region}"
+    environment_name  = "${local.environment_name}"
+    bws_lb_name       = "${local.bws_lb_name}"
+    region            = "${var.region}"
+    slow_latency      = 1
+  }
+}
+
+
+### SNS
+
+resource "aws_sns_topic" "alarm_notification" {
+  name               = "${local.environment_identifier}-${local.mis_app_name}-alarm-notification"
+}
+
+resource "aws_sns_topic_subscription" "alarm_subscription" {
+  count              = "${var.alarms_enabled == "true" ? 1 : 0}"
+  topic_arn          = "${aws_sns_topic.alarm_notification.arn}"
+  protocol           = "lambda"
+  endpoint           = "${aws_lambda_function.notify-ops-slack.arn}"
+}
+
+### Lambda
+
+locals {
+  lambda_name = "${local.environment_identifier}-${local.mis_app_name}-notify-ops-slack"
+}
+
+data "archive_file" "notify-ops-slack" {
+  type               = "zip"
+  source_file        = "${path.module}/lambda/${local.lambda_name}.js"
+  output_path        = "${path.module}/files/${local.lambda_name}zip"
+}
+
+data "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+}
+
+resource "aws_lambda_function" "notify-ops-slack" {
+  filename           = "${data.archive_file.notify-ops-slack.output_path}"
+  function_name      = "${local.lambda_name}"
+  role               = "${data.aws_iam_role.lambda_exec_role.arn}"
+  handler            = "${local.lambda_name}.handler"
+  source_code_hash   = "${base64sha256(file("${data.archive_file.notify-ops-slack.output_path}"))}"
+  runtime            = "nodejs8.10"
+}
+
+resource "aws_lambda_permission" "with_sns" {
+  statement_id        = "AllowExecutionFromSNS"
+  action              = "lambda:InvokeFunction"
+  function_name       = "${aws_lambda_function.notify-ops-slack.arn}"
+  principal           = "sns.amazonaws.com"
+  source_arn          = "${aws_sns_topic.alarm_notification.arn}"
 }
