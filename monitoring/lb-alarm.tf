c05387b696762f6454ba9c290c06b6b8fe137c98
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-10-02 11:52:58 +0200

    adding mis monitoringv2

--- monitoring/lb-alarm.tf
+++ monitoring/lb-alarm.tf
@@ -33,3 +33,21 @@ resource "aws_cloudwatch_metric_alarm" "bws_lb_latency" {
               LoadBalancerName  = "${local.bws_lb_name}"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "bws_lb_spillovercount" {
+  alarm_name                = "${local.bws_lb_name}_lb-spill_over_count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "SpilloverCount"
+  namespace                 = "AWS/ELB"
+  period                    = "300"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors BWS lb Latency"
+  alarm_actions             = [ "${aws_sns_topic.alarm_notification.arn}" ]
+  treat_missing_data        = "notBreaching"
+
+  dimensions {
+              LoadBalancerName  = "${local.bws_lb_name}"
+  }
+}
