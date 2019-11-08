9bf9da81e696019c5f157f07fa5bf48b07d1dbe8
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-11-08 18:00:02 +0100

    adding nextcloud lb alarms and correct check infra jenkins file

--- monitoring/lb-alarm.tf
+++ monitoring/lb-alarm.tf
@@ -1,3 +1,4 @@
+#BWS LB
 resource "aws_cloudwatch_metric_alarm" "bws_lb_unhealthy_hosts" {
   alarm_name                = "${local.bws_lb_name}-lb-unhealthy-hosts-count"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -77,5 +78,62 @@ resource "aws_cloudwatch_log_metric_filter" "SesAuthenticationFail" {
     name      = "EventCount"
     namespace = "smtp"
     value     = "1"
+  }
+}
+
+
+#Nextcloud LB
+
+resource "aws_cloudwatch_metric_alarm" "nextcloud_lb_unhealthy_hosts" {
+  alarm_name                = "${local.nextcloud_lb_name}-lb-unhealthy-hosts-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors Nextcloud lb unhealthy host count"
+  alarm_actions             = [ "${aws_sns_topic.alarm_notification.arn}" ]
+
+  dimensions {
+              LoadBalancerName  = "${local.nextcloud_lb_name}"
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "nextcloud_lb_latency" {
+  alarm_name                = "${local.nextcloud_lb_name}-lb-latency"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "Latency"
+  namespace                 = "AWS/ELB"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors Nextcloud lb Latency"
+  alarm_actions             = [ "${aws_sns_topic.alarm_notification.arn}" ]
+  treat_missing_data        = "notBreaching"
+
+  dimensions {
+              LoadBalancerName  = "${local.nextcloud_lb_name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "nextcloud_lb_spillovercount" {
+  alarm_name                = "${local.nextcloud_lb_name}_lb-spill_over_count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "SpilloverCount"
+  namespace                 = "AWS/ELB"
+  period                    = "300"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors Nextcloud lb Latency"
+  alarm_actions             = [ "${aws_sns_topic.alarm_notification.arn}" ]
+  treat_missing_data        = "notBreaching"
+
+  dimensions {
+              LoadBalancerName  = "${local.nextcloud_lb_name}"
   }
 }
