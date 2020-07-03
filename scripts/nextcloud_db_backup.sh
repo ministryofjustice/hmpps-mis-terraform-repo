90ae3e8279e12bd192ec6fb0ec7c1aa1c43e5be2
eddie nyambudzi <e.nyambudzi@googlemail.com>
2020-07-03 13:34:03 +0200

    update nextcloud db backup scriptv3

--- scripts/nextcloud_db_backup.sh
+++ scripts/nextcloud_db_backup.sh
@@ -146,7 +146,7 @@ elif [ -z "${TG_ENVIRONMENT_TYPE}" ];
 then
     echo "TG_ENVIRONMENT_TYPE argument not supplied."
     exit 1
-elif [ ${JOB_TYPE} = "db-restore"] && [$ -z "$BACKUP_DATE" ];
+elif [ ${JOB_TYPE} = "db-restore" ] && [ -z "$BACKUP_DATE" ];
 then
     echo "JOB_TYPE : db-restore requires date parameter Format: YYYY-MM-DD"
     exit 1
