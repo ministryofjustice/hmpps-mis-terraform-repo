bdabd7322c9d1f96f443e5953c97e1d9c0fa77b2
smjmoj <smjmoj@gmail.com>
2020-07-21 16:38:29 +0100

    Split the MIS DBs into their subsequent directories.

---
+++ database_mis_standbydb2/main.tf
@@ -0,0 +1,129 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = "~> 1.16"
+}
+
+####################################################
+# DATA SOURCE MODULES FROM OTHER TERRAFORM BACKENDS
+####################################################
+#-------------------------------------------------------------
+### Getting the current vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "vpc/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the shared vpc security groups
+#-------------------------------------------------------------
+data "terraform_remote_state" "vpc_security_groups" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "security-groups/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the common details
+#-------------------------------------------------------------
+data "terraform_remote_state" "common" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "${var.environment_type}/common/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the oracledb backup s3 bucket
+#-------------------------------------------------------------
+data "terraform_remote_state" "s3-oracledb-backups" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "s3/oracledb-backups/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the IAM details
+#-------------------------------------------------------------
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "${var.environment_type}/iam/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the security groups details
+#-------------------------------------------------------------
+data "terraform_remote_state" "security-groups" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "${var.environment_type}/security-groups/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the mis-db-1 data
+#-------------------------------------------------------------
+data "terraform_remote_state" "mis-db-1" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket_name}"
+    key    = "${var.environment_type}/database_mis/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+
+
+#-------------------------------------------------------------
+### Getting the latest amazon ami
+#-------------------------------------------------------------
+
+data "aws_ami" "centos_oracle_db" {
+  owners      = ["895523100917"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["HMPPS Delius-Core OracleDB master *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
---
+++ database_mis_standbydb2/variables.tf
@@ -0,0 +1,42 @@
+variable "environment_identifier" {
+  description = "resource label or name"
+}
+
+variable "short_environment_identifier" {
+  description = "shortend resource label or name"
+}
+
+###
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "project_name" {
+  description = "The project name - delius-core"
+}
+
+variable "environment_type" {
+  description = "environment"
+}
+
+variable "remote_state_bucket_name" {
+  description = "Terraform remote state bucket name"
+}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "ansible_vars_mis_db" {
+  description = "Ansible (oracle_db) vars for user_data script "
+  type        = "map"
+}
+
+variable "db_size_mis" {
+  description = "Details of the database resources size"
+  type = "map"
+}
+
+variable "dependencies_bucket_arn" {
+  description = "arn for Delius Depencies S3 bucket in the Engineering AWS Account"
+}
