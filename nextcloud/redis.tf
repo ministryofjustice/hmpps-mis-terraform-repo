c42da2c5abac92d722e68f913cfe57cf4d655c03
enyambudzi <e.nyambudzi@googlemail.com>
2020-08-10 15:57:03 +0200

    Als 1023 (#199)
    * spread redis across all AZ

--- nextcloud/redis.tf
+++ nextcloud/redis.tf
@@ -5,21 +5,30 @@
 resource "aws_elasticache_subnet_group" "nextcloud" {
   name        = "${local.short_environment_identifier}-nextcloud-cache-subnet"
   description = "${local.short_environment_identifier}-nextcloud-cache-subnet"
-  subnet_ids  = ["${data.terraform_remote_state.vpc.vpc_private-subnet-az1}"]
+  subnet_ids = ["${list(
+    data.terraform_remote_state.vpc.vpc_private-subnet-az1,
+    data.terraform_remote_state.vpc.vpc_private-subnet-az2,
+    data.terraform_remote_state.vpc.vpc_private-subnet-az3,
+  )}"]
 }
 
-resource "aws_elasticache_cluster" "nextcloud_cache" {
-  cluster_id           = "${local.mis_app_name}-${local.app_name}"
-  engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.4"
-  port                 = 6379
-  maintenance_window   = "sun:03:00-sun:06:00"
-  security_group_ids   = ["${local.sg_https_out}",]
-  apply_immediately    = "false"
-  snapshot_window      = "00:00-03:00"
-  tags                 = "${var.tags}"
-  subnet_group_name    = "${aws_elasticache_subnet_group.nextcloud.id}"
+resource "aws_elasticache_replication_group" "nextcloud_cache" {
+  replication_group_id          = "${local.mis_app_name}-${local.app_name}-rg"
+  replication_group_description = "${local.mis_app_name}-${local.app_name} replication group"
+  automatic_failover_enabled    = true
+  node_type                     = "${var.nextcloud_redis_node_type}"
+  number_cache_clusters         = "${var.number_cache_clusters}"
+  subnet_group_name             = "${aws_elasticache_subnet_group.nextcloud.name}"
+  security_group_ids            = ["${local.sg_https_out}",]
+  port                          = 6379
+  at_rest_encryption_enabled    = true
+  tags                          = "${var.tags}"
+}
+
+resource "aws_route53_record" "nextcloud_cache_dns" {
+  zone_id = "${local.private_zone_id}"
+  name    = "${local.app_name}-cache"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${aws_elasticache_replication_group.nextcloud_cache.primary_endpoint_address}"]
 }
