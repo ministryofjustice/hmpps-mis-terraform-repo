f0a7b3a662201fd05676ed8b8b30ad0b5c96bbdc
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-11-30 12:43:47 +0000

    moved security group rules to separate resources

--- fsx-integration/output.tf
+++ fsx-integration/output.tf
@@ -1,3 +1,3 @@
 output "mis_fsx_integration_security_group" {
-    value = aws_security_group.mis-fsx-integration.id
+    value = aws_security_group.mis_fsx_integration.id
 }
--- fsx-integration/vpc.tf
+++ fsx-integration/vpc.tf
@@ -1,25 +1,8 @@
 
-resource "aws_security_group" "mis-fsx-integration" {
+resource "aws_security_group" "mis_fsx_integration" {
   name        = local.security_group_name
   description = "security group to allow MIS instances access to fsx filesystem"
   vpc_id      = local.vpc_id
-
-  # internal traffic
-  ingress {
-    description = "ingress internal security group traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    self        = true
-  }
-
-  egress {
-    description = "egress internal security group traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    self        = true
-  }
 
   tags = merge(
     local.tags,
@@ -31,4 +14,24 @@ resource "aws_security_group" "mis-fsx-integration" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_security_group_rule" "mis_fsx_integration_ingress_all_local_sg" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = -1
+  security_group_id        = aws_security_group.mis_fsx_integration.id
+  self                     = true
+  description              = "ingress internal security group traffic"
+}
+
+resource "aws_security_group_rule" "mis_fsx_integration_egress_all_local_sg" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = -1
+  security_group_id        = aws_security_group.mis_fsx_integration.id
+  self                     = true
+  description              = "egress internal security group traffic"
 }
