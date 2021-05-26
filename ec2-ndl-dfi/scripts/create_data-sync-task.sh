71ea3a32c27d5897c4b5a17aeba51cfa3cc397de
eddie nyambudzi <e.nyambudzi@googlemail.com>
2021-05-26 14:47:33 +0100

    update datasync locations paths

--- ec2-ndl-dfi/scripts/create_data-sync-task.sh
+++ ec2-ndl-dfi/scripts/create_data-sync-task.sh
@@ -13,7 +13,7 @@ OPTIONS=VerifyMode="NONE",OverwriteMode="ALWAYS",Atime="BEST_EFFORT",Mtime="PRES
 FSX_USER=$(aws ssm get-parameters --names $USER_PARAM --region $REGION --query "Parameters[0]"."Value" | sed 's:^.\(.*\).$:\1:')                      || exit $?
 FSX_PASS=$(aws ssm get-parameters --with-decryption --names $PASS_PARAM --region $REGION --query "Parameters[0]"."Value" | sed 's:^.\(.*\).$:\1:')    || exit $?
 FSX_ARN=$(aws fsx describe-file-systems | jq -r .FileSystems[0].ResourceARN)  || exit $?
-DESTINATION_LOCATION_ARN=$(aws datasync create-location-fsx-windows --fsx-filesystem-arn "${FSX_ARN}" --security-group-arns "${FSX_SG_ARN}" --user "${FSX_USER}" --password "${FSX_PASS}" --subdirectory "dfinterventions/dfi"   --domain "${FSX_DOMAIN}" --tags "Key=Name,Value=$NAME" --region "${REGION}" | jq -r .LocationArn) || exit $?
+DESTINATION_LOCATION_ARN=$(aws datasync create-location-fsx-windows --fsx-filesystem-arn "${FSX_ARN}" --security-group-arns "${FSX_SG_ARN}" --user "${FSX_USER}" --password "${FSX_PASS}" --subdirectory "dfinterventions"   --domain "${FSX_DOMAIN}" --tags "Key=Name,Value=$NAME" --region "${REGION}" | jq -r .LocationArn) || exit $?
 
 #Create datasync Task
 aws datasync create-task  --source-location-arn ${SOURCE_LOCATION_ARN} --destination-location-arn  ${DESTINATION_LOCATION_ARN} --cloud-watch-log-group-arn ${CLOUDWATCH_LOG_ARN}  --name ${NAME} --options ${OPTIONS}  --region ${REGION} && echo Success || exit $?
