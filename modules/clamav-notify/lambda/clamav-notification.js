e9776f4bf66cbaeb0267ce52e41210dce5fb9b3b
silvianahorga-al <silviana.horga@digital.justice.gov.uk>
2022-03-14 21:09:49 +0000

    Removed commented out lines of code

--- modules/clamav-notify/lambda/clamav-notification.js
+++ modules/clamav-notify/lambda/clamav-notification.js
@@ -7,13 +7,9 @@ exports.handler = function(event, context) {
         const environment = process.env.ENVIRONMENT_TYPE;
         var heading = "ClamAV Notification";
         var bodytext = "ClamAV found some files to be infected. Please review infected folder in DFI bucket for the environment";
-        var channel = "${slack_channel}" // "ndmis-non-prod-alerts";
-        var url_path = "${slack_url}" // "/services/T02DYEB3A/BS16X2JGY/r9e1CJYez7BDmwyliIl7WzLf";
+        var channel = "${slack_channel}"
+        var url_path = "${slack_url}"
         var icon_emoji = ":sign-warning:";
-
-            //if (environment=='prod' )
-            //   channel = "ndmis-alerts";
-
             console.log("Slack channel: " + channel);
 
                var postData = {
