0d463aa5fd36c84796a2649fa15e006b65f84121
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-06-17 14:29:02 +0100

    NIT-1338 reverting s3 commenting out

--- s3buckets/nextcloud-backups.tf
+++ s3buckets/nextcloud-backups.tf
@@ -72,3 +72,85 @@ resource "aws_s3_bucket" "migration_datasync" {
     }
   }
 }
+
+data "aws_iam_policy_document" "migration_datasync" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::891377175249:role/*",
+        "arn:aws:iam::381492116631:role/*",
+        "arn:aws:iam::471112751565:role/*",
+        "arn:aws:iam::590183722357:role/*"
+      ]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.migration_datasync.arn,
+      "${aws_s3_bucket.migration_datasync.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["datasync.amazonaws.com"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.migration_datasync.arn,
+      "${aws_s3_bucket.migration_datasync.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "migration_datasync" {
+  bucket = aws_s3_bucket.migration_datasync.bucket
+  policy = data.aws_iam_policy_document.migration_datasync.json
+}
+
+
+# resource "aws_s3_bucket_policy" "migration_datasync" {
+#   bucket = aws_s3_bucket.migration_datasync.bucket
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           # migration accounts
+#           AWS = [
+#             "arn:aws:iam::891377175249:role/*",
+#             "arn:aws:iam::381492116631:role/*",
+#             "arn:aws:iam::471112751565:role/*",
+#             "arn:aws:iam::590183722357:role/*"
+#           ]
+#         },
+#         Action = "s3:*",
+#         Resource = [
+#           "${aws_s3_bucket.migration_datasync.arn}",
+#           "${aws_s3_bucket.migration_datasync.arn}/*",
+#         ],
+#       },
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "datasync.amazonaws.com"
+#         },
+#         Action = "s3:*",
+#         Resource = [
+#           "${aws_s3_bucket.migration_datasync.arn}",
+#           "${aws_s3_bucket.migration_datasync.arn}/*",
+#         ],
+#       }
+#     ]
+#   })
+# }
