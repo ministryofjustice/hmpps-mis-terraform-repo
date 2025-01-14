79348d6b3ef834c52722aa6c5c598147dfdc564d
Seb Norris <sebastian.j.norris@gmail.com>
2025-01-14 15:33:54 +0000

    remove unneeded vars

--- oracle_migration/variables.tf
+++ oracle_migration/variables.tf
@@ -23,29 +23,7 @@ variable "remote_state_bucket_name" {
   description = "Terraform remote state bucket name"
 }
 
-variable "ansible_vars_mis_db" {
-  description = "Ansible (oracle_db) vars for user_data script "
-  type        = map(string)
-}
-
-variable "db_size_mis" {
-  description = "Details of the database resources size"
-  type        = map(string)
-}
-
-variable "dependencies_bucket_arn" {
-  description = "arn for Delius Depencies S3 bucket in the Engineering AWS Account"
-}
-
 variable "environment_name" {
   type = string
 }
 
-variable "database_high_availability_count" {
-  description = "number of standby databases"
-  type        = map(number)
-}
-
-variable "delius_overide_autostop_tags" {
-  default = "Phase1"
-}
