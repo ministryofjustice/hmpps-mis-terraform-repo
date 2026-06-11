35f4a8de3bbeb245a2aa781ca95c746339e9c4c8
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2026-06-11 16:17:51 +0100

    add new DNS entry for mis-db-2

--- oracle_migration/migration.tf
+++ oracle_migration/migration.tf
@@ -15,7 +15,7 @@ locals {
     "delius-mis-dev"  = ["mis-db-1", "misboe-db-1", "misdsd-db-1"]
     "delius-test"     = []
     "delius-stage"    = ["mis-db-1", "misboe-db-1", "misdsd-db-1"]
-    "delius-pre-prod" = ["mis-db-1"]
+    "delius-pre-prod" = ["mis-db-1", "mis-db-2"]
   }
 }
 
@@ -35,6 +35,24 @@ resource "aws_route53_record" "mis_db1_migration_public" {
   type    = "CNAME"
   ttl     = "300"
   records = ["delius-mis-${local.legacy_to_mp_env[var.environment_name]}-mis-db-1.delius-mis.hmpps-${local.legacy_to_mp_vpc[var.environment_name]}.modernisation-platform.service.justice.gov.uk"]
+
+}
+resource "aws_route53_record" "mis_db2_migration_public" {
+  count   = contains(local.migrated_envs[var.environment_name], "mis-db-2") ? 1 : 0
+  zone_id = data.terraform_remote_state.vpc.outputs.public_zone_id
+  name    = "mis-db-2"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["delius-mis-${local.legacy_to_mp_env[var.environment_name]}-mis-db-2.delius-mis.hmpps-${local.legacy_to_mp_vpc[var.environment_name]}.modernisation-platform.service.justice.gov.uk"]
+}
+
+resource "aws_route53_record" "mis_db2_migration_internal" {
+  count   = contains(local.migrated_envs[var.environment_name], "mis-db-2") ? 1 : 0
+  zone_id = data.terraform_remote_state.vpc.outputs.private_zone_id
+  name    = "mis-db-2"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["delius-mis-${local.legacy_to_mp_env[var.environment_name]}-mis-db-2.delius-mis.hmpps-${local.legacy_to_mp_vpc[var.environment_name]}.modernisation-platform.service.justice.gov.uk"]
 }
 
 resource "aws_route53_record" "boe_db1_migration_internal" {
